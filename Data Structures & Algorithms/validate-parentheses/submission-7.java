b904a0002dba6004bb77b20eeacf3dcffb0ecd53
Mussie <102823896+Mussie3@users.noreply.github.com>
2026-04-13 11:44:56 +0300

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] c = s.toCharArray();
+        Stack<Character> stack =  new Stack<>();
+        Map<Character, Character> dict = new HashMap<>();
+        dict.put(')', '(');
+        dict.put(']', '[');
+        dict.put('}', '{');
+        for(int i = 0; i < c.length; i++){
+            if(!stack.isEmpty() && stack.peek() == dict.get(c[i]) ){
+                stack.pop();
+            } else {
+                stack.push(c[i]);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
